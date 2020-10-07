c5c5a085774115d9700e5db2c2d470dad73881d9
Clayton Campos <clayton.almeida.campos@gmail.com>
2020-10-07 09:07:23 -0300

    divisão em components

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React from "react";
+import SideBarNav from "./components/SideBarNav"
+import "../src/app.css";
+import '../src/geral.css';
+import TopBarNav from "./components/TopBarNav";
+import Footer from "./components/Footer";
+import Cards from "./components/Cards";
+import CardCentral from "./components/CardCentral";
+import Cardcategorias from "./components/CardCategorias";
 
-function App() {
+export default function App() {
   return (
-    <h1>teste</h1>
+    <>
+      <TopBarNav />
+      <div className="container-geral">
 
+        <div className="container-sidebar">
+          <SideBarNav />
+        </div>
+
+        <div className="container-cards">
+          <Cards />
+          <CardCentral />
+          <Cardcategorias />
+
+
+
+
+
+          <div className="footer">
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
